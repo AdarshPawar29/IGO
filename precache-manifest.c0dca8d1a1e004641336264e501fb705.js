d114ad7c83bc6b8c2e03f5c6640d46875628179a
Adarsh Pawar <adarsh.pawar@bcone.com>
2020-02-06 17:20:06 +0530

    Updates

---
+++ precache-manifest.c0dca8d1a1e004641336264e501fb705.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c76a0cd2766846682468b0ba3561bc7",
+    "url": "/IGO/index.html"
+  },
+  {
+    "revision": "60eb464bfcca3241f49a",
+    "url": "/IGO/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "079f681c989171d1ef3b",
+    "url": "/IGO/static/css/main.63b94e12.chunk.css"
+  },
+  {
+    "revision": "60eb464bfcca3241f49a",
+    "url": "/IGO/static/js/2.14387353.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/IGO/static/js/2.14387353.chunk.js.LICENSE"
+  },
+  {
+    "revision": "079f681c989171d1ef3b",
+    "url": "/IGO/static/js/main.bd88e2c0.chunk.js"
+  },
+  {
+    "revision": "90b8f3c404c6864dcb30",
+    "url": "/IGO/static/js/runtime-main.e28d1f37.js"
+  },
+  {
+    "revision": "c65565235893b3dea4e6d562190d0ab5",
+    "url": "/IGO/static/media/bg.c6556523.gif"
+  },
+  {
+    "revision": "1e9441f6b4459fdc55489438337bb5c4",
+    "url": "/IGO/static/media/counter-purple2.1e9441f6.gif"
+  },
+  {
+    "revision": "bc1ceaaefedf55ac874a9d193494e8ce",
+    "url": "/IGO/static/media/divider.bc1ceaae.gif"
+  },
+  {
+    "revision": "94bf8caaf9ad63c0125b3399f1e0b2b0",
+    "url": "/IGO/static/media/email.94bf8caa.gif"
+  },
+  {
+    "revision": "e5c5a5b140a59371d0fea9cb429c770f",
+    "url": "/IGO/static/media/kree.e5c5a5b1.gif"
+  }
+]);
